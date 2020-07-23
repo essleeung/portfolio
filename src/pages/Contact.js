91acf5ca8aa31cfe6e8e34947bc29166020785d3
essleeung <essleeung@gmail.com>
2020-07-22 23:11:34 -0400

    got the wave to workkkk

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,16 +7,81 @@ import { Col, Container, Row } from 'react-bootstrap';
 const Contact = props => {
 
     return (
-        <div className="wave-container">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 450" >
-                <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-                    <stop offset="0%" stop-color="#8C52FF" />
-                    <stop offset="50%" stop-color="#76CCCF" />
-                    <stop offset="100%" stop-color="#8C52FF" />
-                </linearGradient>
-                <path fill="url(#grad1)" fill-opacity="0.7" d="M0,96L48,128C96,160,192,224,288,240C384,256,480,224,576,181.3C672,139,768,85,864,96C960,107,1056,181,1152,186.7C1248,192,1344,128,1392,96L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
-                </path>
-            </svg>
+        <div>
+            <div className="waves">
+                <svg id="wave-svg" width="100%" height="400px" fill="none" version="1.1"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                        <stop offset="0%" stop-color="#8C52FF" />
+                        <stop offset="50%" stop-color="#76CCCF" />
+                        <stop offset="100%" stop-color="#8C52FF" />
+                    </linearGradient>
+                    <path
+                        fill="url(#grad1)"
+                        d="
+          M0 67
+          C 273,183
+            822,-40
+            1920.00,106 
+        
+          V 359 
+          H 0 
+          V 67
+          Z">
+                        <animate
+                            repeatCount="indefinite"
+                            fill="url(#grad1)"
+                            attributeName="d"
+                            dur="15s"
+                            attributeType="XML"
+                            values="
+            M0 77 
+            C 473,283
+              822,-40
+              1920,116 
+            
+            V 359 
+            H 0 
+            V 67 
+            Z; 
+
+            M0 77 
+            C 473,-40
+              1222,283
+              1920,136 
+            
+            V 359 
+            H 0 
+            V 67 
+            Z; 
+
+            M0 77 
+            C 973,260
+              1722,-53
+              1920,120 
+            
+            V 359 
+            H 0 
+            V 67 
+            Z; 
+
+            M0 77 
+            C 473,283
+              822,-40
+              1920,116 
+            
+            V 359 
+            H 0 
+            V 67 
+            Z
+            ">
+                        </animate>
+                    </path>
+                </svg>
+            </div>
+
+
+
             <Container className="max-vh contact-container">
                 <h1>Feel that <strong>tingling</strong> in your fingertips?</h1>
                 <p>That's the itch to drop me a note via the form below or directly at <em>essleeung[at]gmail.com</em>.</p>
@@ -32,7 +97,7 @@ const Contact = props => {
                     </Row>
                 </Container>
             </Container >
-        </div>
+        </div >
     )
 }
 
