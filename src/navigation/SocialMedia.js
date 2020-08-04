bea428bf89b548f330dbc318f7a4009e2e7dd738
essleeung <essleeung@gmail.com>
2020-08-03 21:58:10 -0400

    added box shadow and changed cv icon to text

--- src/navigation/SocialMedia.js
+++ src/navigation/SocialMedia.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub, faCodepen, faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faFile } from '@fortawesome/free-solid-svg-icons'
+// import { faFile } from '@fortawesome/free-solid-svg-icons'
 import pdf from '../assets/sharon_lee_resume.pdf'
 
 const SocialMedia = props => {
@@ -11,7 +11,7 @@ const SocialMedia = props => {
             <div><a href="https://github.com/essleeung" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="icon-social" icon={faGithub} /></a></div>
             <div><a href="https://codepen.io/essleeung" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="icon-social" icon={faCodepen} /></a></div>
             <div><a href="https://twitter.com/SharonsmLee" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="icon-social" icon={faTwitter} /></a></div>
-            <div><a href={pdf} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFile} /></a></div>
+            <div><a href={pdf} target="_blank" rel="noopener noreferrer" className="cv icon-social">CV</a></div>
         </div>
     )
 }
